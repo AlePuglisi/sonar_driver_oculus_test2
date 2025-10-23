92bacea5c8d6b0c6f8d5dffff02c16c197c6aab6
AlePuglisi <puglisialessandro27@gmail.com>
2025-10-23 14:00:31 +0200

    feat: first draft of ros2 node for communicating with sonar

--- include/sonar_driver_oculus/sonar_driver_oculus.h
+++ include/sonar_driver_oculus/sonar_driver_oculus.h
@@ -15,8 +15,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/compressed_image.hpp>
-#include <sonar_oculus_interface/msg/oculus_fire.hpp>
-#include <sonar_oculus_interface/msg/oculus_ping.hpp>
+// #include <sonar_oculus_interface/msg/oculus_fire.hpp>
+// #include <sonar_oculus_interface/msg/oculus_ping.hpp>
 
 using namespace cv;
 
@@ -36,36 +36,48 @@ class SonarDriverOculusNode : public rclcpp::Node
        */
       rcl_interfaces::msg::SetParametersResult reconfigureSonarCallback(
          const std::vector<rclcpp::Parameter> &params);
-
+      /**
+       * @brief This callback uses the sonar_driver to periodically collect sonar data 
+       *        
+       */
+      void loopSonarCallback();
+      /**
+       * @brief This callback uses the sonar_driver to periodically Fire sonar 
+       *        
+       */
+      void loopFireCallback();
 
       // ROS Publishers
-      rclcpp::Publisher<sonar_oculus_interface::msg::OculusPing>::SharedPtr ping_pub_;
+      rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr sonar_image_pub_;
 
       // Parameter callback handle (for dynamic reconfiguration)
       OnSetParametersCallbackHandle::SharedPtr param_callback_handle_;
 
       // --- DRIVER VARIABLES --- //       
       // ROS Timer for the Ping reader loop 
-      rclcpp::TimerBase::SharedPtr sonar_timer_;
-
+      rclcpp::TimerBase::SharedPtr sonar_update_timer_;
+      rclcpp::TimerBase::SharedPtr sonar_fire_timer_;
       // Messages 
-      sensor_msgs::msg::Image sonar_image;
-      sonar_oculus_interface::msg::OculusFire fire_msg;
-      sonar_oculus_interface::msg::OculusPing ping_msg;
+      sensor_msgs::msg::Image sonar_image_msg;
+      // sonar_oculus_interface::msg::OculusFire fire_msg;
+      // sonar_oculus_interface::msg::OculusPing ping_msg;
 
       // Main interface with Sonar  
-      OsClientCtrl sonar;               // User facing API to interact with Sonar HW 
-      OculusPartNumberType partNumber;  // Data type to represent HW partNumber based on Oculus Model 
+      optional<OsDriver> sonar_driver; 
+      FireConfig2 sonar_fire_config; 
 
       // Sonar Configuration (From ROS params)
       std::string model;
       std::string frame_id; 
-      int mode;
-      int ping_rate;
-      double range;
-      double gain;
-      double soundspeed;
-      double salinity;
+      int mode                 = 1; 
+      int pingRate             = 5;
+      double range             = 10.0; 
+      double gain              = 20.0; 
+      double speedOfSound      = 0.0; 
+      double salinity          = 0.0; 
+      bool gainAssist          = false;  
+      uint8_t gammaCorrection  = 0x7f; 
+      uint8_t netSpeedLimit    = 0xff; 
 
 };
 
--- src/sonar_driver_oculus.cpp
+++ src/sonar_driver_oculus.cpp
@@ -2,107 +2,158 @@
 
 SonarDriverOculusNode::SonarDriverOculusNode()
 : Node("sonar_oculus"),
-   model("M3000d"), frame_id("sonar"), mode(1), ping_rate(3), range(10.0), gain(20.0), soundspeed(0.0), salinity(0.0)
+   model("M3000d"), frame_id("sonar"), mode(1), pingRate(3), range(10.0), gain(20.0), 
+   speedOfSound(0.0), salinity(0.0), gainAssist(false), gammaCorrection(0x7f), netSpeedLimit(0xff) 
 {
     // Declare and get parameters with defaults
     this->declare_parameter<std::string>("model", model);
     this->declare_parameter<std::string>("frame_id", frame_id);
     this->declare_parameter<int>("Mode", mode);
-    this->declare_parameter<int>("PingRate", ping_rate);
-    this->declare_parameter<double>("Range", range);
-    this->declare_parameter<double>("Gain", gain);
-    this->declare_parameter<double>("Salinity", salinity);
+    this->declare_parameter<int>("pingRate", pingRate);
+    this->declare_parameter<double>("range", range);
+    this->declare_parameter<double>("gain", gain);
+    this->declare_parameter<double>("speedOfSound", speedOfSound);
+    this->declare_parameter<double>("salinity", salinity);
+    this->declare_parameter<bool>("gainAssist", gainAssist);
+    this->declare_parameter<uint8_t>("gammaCorrection", gammaCorrection);
+    this->declare_parameter<uint8_t>("netSpeedLimit", netSpeedLimit);
 
     // Define callback for dynamic parameter changes
     param_callback_handle_ = this->add_on_set_parameters_callback(
-        std::bind(&SonarOculusNode::reconfigureSonarCallback, this, std::placeholders::_1));
+        std::bind(&SonarDriverOculusNode::reconfigureSonarCallback, this, std::placeholders::_1));
 
     // Define Oculus Ping publisher
-    ping_pub_ = this->create_publisher<sonar_oculus_interface::msg::OculusPing>(
-        "/sonar_oculus_node/" + model + "/ping", 10);
+    // ping_pub_ = this->create_publisher<sonar_oculus_interface::msg::OculusPing>(
+    //     "/sonar_oculus_node/" + model + "/ping", 10);
+    sonar_image_pub_ = this->create_publisher<sensor_msgs::msg::Image>(
+        "/sonar_oculus_node/" + model + "/image", 10);
 
-    // Initialize communication: UDP Network Search (If ip unknown) 
-    searchSonar();
-    // Connect to Sonar, based on previous search
-    connectToSonar();
+    sonar_fire_config.mode = mode; 
+    sonar_fire_config.pingRate = pingRate; 
+    sonar_fire_config.range = range; 
+    sonar_fire_config.gain = gain; 
+    sonar_fire_config.speedOfSound = speedOfSound; 
+    sonar_fire_config.salinity = salinity; 
+    sonar_fire_config.gainAssist = gainAssist; 
+    sonar_fire_config.gammaCorrection = gammaCorrection; 
+    sonar_fire_config.netSpeedLimit = netSpeedLimit; 
 
-    RCLCPP_INFO(this->get_logger(), "Sonar Oculus node initialized.");
-    RCLCPP_INFO(this->get_logger(), "Starting read loop.");
+    // delayed construction of the object 
+    RCLCPP_INFO(this->get_logger(), "Creating Sonar Oculus Driver instance...");
+    sonar_driver.emplace(IP_ADDR, sonar_fire_config); 
 
     // Setup timer loop (50 Hz here)
-    sonar_timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(20), std::bind(&SonarOculusNode::publishPing, this));
+    if(sonar_driver->sonarConnected) {
+        RCLCPP_INFO(this->get_logger(), "Sonar Oculus node initialized.");
+
+        RCLCPP_INFO(this->get_logger(), "Start Sonar Oculus data collection and Fire Loop");
+        RCLCPP_INFO(this->get_logger(), "Sending first Fire request");
+        sonar_driver->fireSonar(); 
+        sonar_update_timer_ = this->create_wall_timer(
+            std::chrono::nanoseconds(sonar_driver->readPeriod), std::bind(&SonarDriverOculusNode::loopSonarCallback, this));
+        sonar_fire_timer_ = this->create_wall_timer(
+            std::chrono::nanoseconds(sonar_driver->firePeriod), std::bind(&SonarDriverOculusNode::loopFireCallback, this));
+    } else { 
+        RCLCPP_INFO(this->get_logger(), "Cannot Start Sonar Oculus data collection, Sonar not connected...");
+        rclcpp::shutdown(); 
+    }
 
 }
 
 SonarDriverOculusNode::~SonarDriverOculusNode()
-{
-    // Disconnect from sonar
-    sonar.Disconnect();
-    // close TCP connection 
-    close(sockTCP);
-    
+{   
     RCLCPP_INFO(this->get_logger(), "Disconnecting from Sonar.");
 }
 
 rcl_interfaces::msg::SetParametersResult
-SonarOculusNode::reconfigureSonarCallback(const std::vector<rclcpp::Parameter> &params)
+SonarDriverOculusNode::reconfigureSonarCallback(const std::vector<rclcpp::Parameter> &params)
 {
     rcl_interfaces::msg::SetParametersResult result;
     result.successful = true;
 
     // Cast parameters to the correct data type 
     for (const auto &param : params) {
-        if (param.get_name() == "Mode") {
+        if (param.get_name() == "mode") {
             mode = param.as_int();
-        } else if (param.get_name() == "PingRate") {
-            ping_rate = param.as_int();
-        } else if (param.get_name() == "Gain") {
-            gain = param.as_double();
-        } else if (param.get_name() == "Range") {
+        } else if (param.get_name() == "pingRate") {
+            pingRate = param.as_int();
+        } else if (param.get_name() == "range") {
             range = param.as_double();
-        } else if (param.get_name() == "Salinity") {
+        } else if (param.get_name() == "gain") {
+            gain = param.as_double();
+        }  else if (param.get_name() == "speedOfSound") {
+            speedOfSound = param.as_double();
+        }  else if (param.get_name() == "salinity") {
             salinity = param.as_double();
+        }  else if (param.get_name() == "gainAssist") {
+            gainAssist = param.as_bool();
+        } else if (param.get_name() == "gammaCorrection") {
+            gammaCorrection = param.as_int();
+        } else if (param.get_name() == "netSpeedLimit") {
+            netSpeedLimit = param.as_int();
         }
     }
 
-    // Clamb HW configuration based on the Sonar Oculus Model 
-    // Specifications here: https://www.blueprintsubsea.com/oculus/oculus-m-series
-    if (partNumber == OculusPartNumberType::partNumberM750d) {
-        if (mode == 1)
-            range = std::max(0.1, std::min(range, 120.0));
-        else if (mode == 2)
-            range = std::max(0.1, std::min(range, 40.0));
-    }
-    if (partNumber == OculusPartNumberType::partNumberM1200d) {
-        range = std::max(0.1, std::min(range, 30.0));
-    }
-    if (partNumber == OculusPartNumberType::partNumberM3000d) {
-        if (mode == 1) {
-            range = std::max(0.1, std::min(range, 30.0));
-        }
-        else if (mode == 2) {
-            range = std::max(0.1, std::min(range, 5.0));
-        }
-    }
+    sonar_fire_config.mode = mode; 
+    sonar_fire_config.pingRate = pingRate; 
+    sonar_fire_config.range = range; 
+    sonar_fire_config.gain = gain; 
+    sonar_fire_config.speedOfSound = speedOfSound; 
+    sonar_fire_config.salinity = salinity; 
+    sonar_fire_config.gainAssist = gainAssist; 
+    sonar_fire_config.gammaCorrection = gammaCorrection; 
+    sonar_fire_config.netSpeedLimit = netSpeedLimit; 
 
-    sonar.Fire(mode, ping_rate, range, gain, soundspeed, salinity);
-
-    RCLCPP_INFO(this->get_logger(), 
-        "Reconfigured params -> Mode:%d PingRate:%d Range:%.2f Gain:%.2f Salinity:%.2f",
-        mode, ping_rate, range, gain, salinity);
+    sonar_driver->initializeSonar(sonar_fire_config);
 
     return result;
 }
+
+void SonarDriverOculusNode::loopSonarCallback()
+{
+    // Update image from the driver API 
+    sonar_driver->updateImage(); 
+    // Check if No image still received 
+    if (sonar_driver->sonarImage == nullptr){
+        RCLCPP_INFO(this->get_logger(), "No Image yet received");
+        return; 
+    } else { // Republish sonar image at the publish rate 
+        sonar_image_msg.header.frame_id = frame_id;
+        sonar_image_msg.header.stamp = this->get_clock()->now();
+
+        sonar_image_msg.height = sonar_driver->sonarImageHeight;
+        sonar_image_msg.width = sonar_driver->sonarImageWidth;
+        sonar_image_msg.encoding = "8UC1";
+        sonar_image_msg.is_bigendian = 0;
+        sonar_image_msg.step = sonar_driver->sonarImageWidth;
+
+        sonar_image_msg.data.resize(sonar_driver->sonarImageSize); 
+        memcpy(sonar_image_msg.data.data(), sonar_driver->sonarImage, sonar_driver->sonarImageSize);
+
+        sonar_image_pub_->publish(sonar_image_msg); 
+
+    }
+
+}
+
+void SonarDriverOculusNode::loopFireCallback()
+{
+    RCLCPP_INFO(this->get_logger(), "New Fire request sent");
+    sonar_driver->fireSonar(); 
+}   
+
+
 
 // Main program for listening to sonar
 int main(int argc, char **argv) 
 {
   rclcpp::init(argc, argv);
   auto sonar_driver_oculus_node = std::make_shared<SonarDriverOculusNode>();
-  rclcpp::spin(sonar_driver_oculus_node);
+  if(rclcpp::ok()){
+    rclcpp::spin(sonar_driver_oculus_node);
 
-  rclcpp::shutdown();
+    rclcpp::shutdown();
+  }
 
   return 0;
 } 
